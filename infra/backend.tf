ca414a5fc5beffd9f1e6dd27e212196450e6f0cb
Fabio Marques <fabio.marquezzx@gmail.com>
2024-04-17 22:34:03 -0300

    update

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = var.bucket_backend_name
-    key            = var.lambda_name + "/terraform.tfstate"
+    key            = "fiap_pedidos_ms/terraform.tfstate"
     region         = "us-east-1"
     
   }
