a5e58f3961a30876354086061a03dfcebbfd3b6e
Fabio Marques <fabio.marquezzx@gmail.com>
2024-04-28 15:27:09 -0300

    fix: fixing unit test

--- app/test/automated/steps/CreateOrder.steps.js
+++ app/test/automated/steps/CreateOrder.steps.js
@@ -6,15 +6,21 @@ const { Database } = require("../../../src/config/DatabaseConfig.js").default;
 
 let productId;
 let order;
+let clientCpf;
 
 const apiPath = "https://6wp2xidb1e.execute-api.us-east-1.amazonaws.com/prod/fiap-lanches"
 
 Before(async function () {
     const connection = new Database();
-    
+
     try {
         const select = "SELECT id FROM public.product LIMIT 1;"
+        const selectClient = "SELECT id, cpf, name, email FROM public.client LIMIT 1;"
+
         let result = await connection.query(select, null).then(resp => resp);
+        let resultClient = await connection.query(selectClient, null).then(resp => resp);
+
+        clientCpf = resultClient.rows
         productId = result.rows
     } catch (err) {
         console.error('Error in getting product:', err.stack);
@@ -41,6 +47,7 @@ After(async function () {
 When('I send a POST request to {string} with the following details with a random product ID:', async function (path, dataTable) {
     const table = dataTable.rowsHash()
     table.productsIds = [productId[0].id]
+    table.clientCpf = clientCpf[0].cpf
 
     console.log(table)
     return axios.post(apiPath + path, table)
@@ -48,38 +55,32 @@ When('I send a POST request to {string} with the following details with a random
             if (response.status !== 200) {
                 throw new Error('Error in creating order: ' + response.statusText);
             }
-            console.log(response)
+
+            return response.data
         })
         .then(data => {
-            // console.log('Product recovered:', id);
-            // products.push(data.id); //
+            this.order = data
         })
-        .catch(err => {
-            console.log("err")
-            console.log(err)
-        });
 });
 
 Then('the response should have status {int}', function (expectedStatus) {
-    // Implementação dos passos para verificar o status da resposta
-    // expect(response.status).to.equal(expectedStatus);
+    expect(this.order.statusCode).to.equal(expectedStatus);
 });
 
 Then('should return a unique order ID', function () {
-    // Implementação dos passos para verificar se um ID de pedido único foi retornado
-    // expect(orderId).to.be.a('string');
+    expect(this.order.orderId).to.be.a('string');
 });
 
 Then('the order should be registered with the following details:', async function (dataTable) {
-    // const expectedOrderData = dataTable.rowsHash();
+    const expectedOrderData = dataTable.rowsHash();
 
-    console.log(dataTable)
-    // // Lógica para recuperar os detalhes do pedido com base no orderId
-    // const response = await axios.get(`/orders/${orderId}`);
-    // const orderData = response.data;
+    const select = "SELECT * FROM public.product WHERE id = ;" + this.order.orderId;
+    let result = await connection.query(select, null).then(resp => resp);
+    orderData = result.rows[0]
 
-    // // Implementação dos passos para verificar os detalhes do pedido
-    // expect(orderData.clientId).to.equal(expectedOrderData.clientId);
+    console.log(orderData)
+
+    // expect(orderData.cpf).to.equal(this.clientCpf[0].cpf);
     // expect(orderData.value).to.equal(expectedOrderData.value);
     // expect(orderData.status).to.equal(expectedOrderData.status);
 });
