e01711a558bf98ca31ee02497d173b9f0a2259a4
Fabio Marques <fabio.marquezzx@gmail.com>
2024-07-13 18:57:07 -0300

    .

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "backend-fiap"
+    bucket         = "backend-projeto"
     key            = "fiap_pedidos_ms/terraform.tfstate"
     region         = "us-east-2"
     
